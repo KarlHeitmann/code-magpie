604aa6422ceda0012a26b03352f3e5c03124bea6
Karl Heitmann <khf1988@gmail.com>
2022-03-24 01:01:47 -0300

    added invoices

--- magpie_react/src/routes/invoices.jsx
+++ magpie_react/src/routes/invoices.jsx
@@ -1,11 +1,20 @@
+import { useRef, useEffect, useState } from 'react'
+import { gsap } from "gsap";
+// import { PixiPlugin } from "gsap/PixiPlugin.js";
 import TheNavbar from '../misc/the_navbar';
 
 export default function Invoices() {
+  const boxRef = useRef();
+  useEffect(() => {
+    gsap.to(boxRef.current, { rotation: "+=360" });
+  });
+
   return (
     <>
       <TheNavbar />
       <main style={{ padding: "1rem 0" }}>
         <h2>Invoices</h2>
+        <div className="box" ref={boxRef}>Hello</div>
       </main>
     </>
   );
