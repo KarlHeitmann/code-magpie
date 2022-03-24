92361927051dd84649d674cc861b52f46c88c938
Karl Heitmann <khf1988@gmail.com>
2022-03-24 00:30:40 -0300

    added a simple animation

--- magpie_react/src/App.jsx
+++ magpie_react/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { gsap } from "gsap";
+// import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const boxRef = useRef();
+
+  // wait until DOM has been rendered
+  useEffect(() => {
+    gsap.to(boxRef.current, { rotation: "+=360" });
+  });
+
+  /*
+  let scene1 = gsap.timeline()
+  ScrollTrigger.create({
+    animation: scene1,
+    trigger: ".scrollElement",
+    start: "top top",
+    end: "45% 100%",
+    scrub: 3
+  })
+//const boxRef = useRef();
+*/
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <div className="box" ref={boxRef}>Hello</div>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
